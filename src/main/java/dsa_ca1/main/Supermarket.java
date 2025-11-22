1d0986280c3654b2b971b8c03ab3a3e02b316ae1
KarlEstante <karlandrei545@gmail.com>
2025-11-22 15:14:08 +0000

    11 - GUI tested (not finished)

---
+++ src/main/java/dsa_ca1/main/Supermarket.java
@@ -0,0 +1,24 @@
+package dsa_ca1.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Supermarket extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dsa_ca1/views/supermarket.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600); // adjust size
+        scene.getStylesheets().add(getClass().getResource("/dsa_ca1/views/Supermarket.css").toExternalForm());
+
+        primaryStage.setTitle("Supermarket Management");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,11 @@
-module org.example.demo {
+module dsa_ca1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires xstream;
 
-
-    opens org.example.demo to javafx.fxml;
-    opens dsa_ca1 to javafx.fxml;
-    exports dsa_ca1.controllers;
     opens dsa_ca1.controllers to javafx.fxml;
-    exports dsa_ca1.main;
     opens dsa_ca1.main to javafx.fxml;
+
+    exports dsa_ca1.controllers;
+    exports dsa_ca1.main;
 }
